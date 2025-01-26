efad864cd639b93e93e659b123bfd7d2887bcde4
Justin Hollister <hollisterjustin2025@serrahs.com>
2025-01-26 01:15:28 -0800

    removed lumache

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Team3045'
+copyright = '2024, GearGremlins'
+author = 'GearGremlins'
 
 release = '0.1'
 version = '0.1.0'
